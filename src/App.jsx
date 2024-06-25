c8eb021a1d5dacf6e553d4f963908d5040d88fe1
KetaLoca <mianramar@gmail.com>
2024-06-25 12:30:15 +0200

    Navigate implementado

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,22 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import reactLogo from "./assets/react.svg";
 import viteLogo from "/vite.svg";
 import "./App.css";
+
+const NAVIGATION_EVENT = "pushstate";
+
+function navigate(href) {
+  window.history.pushState({}, "", href);
+  const navigationEvent = new Event(NAVIGATION_EVENT);
+  window.dispatchEvent(navigationEvent);
+}
 
 function HomePage() {
   return (
     <>
       <h1>Home</h1>
       <p>Esta es una página de ejemplo para crear un React Router desde cero</p>
-      <a href="/about">Ir a Sobre nosotros</a>
+      <button onClick={() => navigate('/about')}>Ir a Sobre nosotros</button>
     </>
   );
 }
@@ -22,15 +30,28 @@ function AboutPage() {
           src="https://i0.wp.com/puppis.blog/wp-content/uploads/2022/02/abc-cuidado-de-los-gatos-min.jpg?resize=521%2C346&ssl=1"
           alt="Foto gatito"
         />
-        <p>Holaaa, estamo creando un clon de React Router</p>
+        <p>Holaaa, estamos creando un clon de React Router</p>
       </div>
-      <a href="/">Ir a la Home</a>
+      <button onClick={() => navigate('/')}>Ir a la Home</button>
     </>
   );
 }
 
 function App() {
   const [currentPath, setCurrentPath] = useState(window.location.pathname);
+
+  useEffect(() => {
+    const onLocationChange = () => {
+      setCurrentPath(window.location.pathname);
+    };
+
+    window.addEventListener(NAVIGATION_EVENT, onLocationChange);
+
+    return () => {
+      window.removeEventListener(NAVIGATION_EVENT, onLocationChange);
+    };
+  }, []);
+
   return (
     <main>
       {currentPath == "/" && <HomePage />}
