ce5f7a50f953db5eee1e5c33a18383aa6f41c645
KetaLoca <mianramar@gmail.com>
2024-06-25 13:52:11 +0200

    Componente de prueba añadido en el router

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,26 @@
 import "./App.css";
-import { Router } from "./Router";
 import HomePage from "./pages/Home";
 import AboutPage from "./pages/About";
+
+import { Router } from "./Router";
+import { Link } from "./Link";
 
 const routes = [
   { path: "/", Component: HomePage },
   { path: "/about", Component: AboutPage },
+  {
+    path: "/ketatesting",
+    Component: () => (
+      <div>
+        <h1>Probando las rutas</h1>
+        <img
+          src="https://i.pinimg.com/236x/b0/52/fd/b052fd86b510887c2e1e907cdd5fd60f.jpg"
+          alt="Gatito tryahrd"
+        />
+        <p><Link to="/">Volver a la Home</Link></p>
+      </div>
+    ),
+  },
 ];
 
 function App() {
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -5,7 +5,10 @@ export default function HomePage() {
     <>
       <h1>Home</h1>
       <p>Esta es una página de ejemplo para crear un React Router desde cero</p>
-      <Link to='/about'>Ir a Sobre nosotros</Link>
+      <Link to="/about">Ir a Sobre nosotros</Link>
+      <br></br>
+      <br></br>
+      <Link to="/ketatesting">Testing Page Here</Link>
     </>
   );
 }
